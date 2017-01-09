eb1cebb5e5a0d059220ba457b0b655884dcd3a32
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-01-09 16:27:44 -0700

    Added debounce.

--- assets/js/controllers/EntryHomeController.js
+++ assets/js/controllers/EntryHomeController.js
@@ -2,53 +2,11 @@
   'use strict';
 
   angular.module('leansite')
-    .controller('EntryHomeController', ['$scope', '$mdDialog', '$location', '$anchorScroll', '_entryService', 'BROADCAST', EntryHomeController]);
+    .controller('EntryHomeController', ['$scope', '$mdDialog', '$location', '$anchorScroll', '$q', '_entryService', 'BROADCAST', EntryHomeController]);
 
-  function EntryHomeController($scope, $mdDialog, $location, $anchorScroll, _entryService, BROADCAST) {
-    $anchorScroll();
+  function EntryHomeController($scope, $mdDialog, $location, $anchorScroll, $q, _entryService, BROADCAST) {
+    $anchorScroll();  // Scroll to anchor if present
     var vm = this;
-    vm.recent = [];
-    vm.results = [];
-
-    vm.isSearching = false;
-
-    vm.search = "";
-
-    vm.loadRecent = function () {
-      _entryService.getRecent(10)
-        .then(function (response) {
-          vm.recent = response.data;
-        })
-        .catch(function (err) {
-          if (BROADCAST.loggingLevel == "DEBUG") {
-            $rootScope.$broadcast(BROADCAST.error, JSON.stringify(err));
-          } else {
-            $rootScope.$broadcast(BROADCAST.error, "There was an error loading recent questions. Please try again...");
-          }
-        });
-    }
-
-    $scope.$watch('vm.search', function(newV, oldV){
-      console.log("query", newV);
-      if(newV == ''){
-        vm.results = [];
-        vm.isSearching = false;
-      } else {
-        vm.isSearching = true;
-        vm.query(newV);
-      }
-    });
-
-    vm.query = function(query){
-      _entryService.query(query)
-      .then(function(response){
-        vm.results = response.data;
-      })
-      .catch(function(err){
-        console.log(err);
-      })
-    }
-
     vm.postQuestion = function (_owner) {
       $mdDialog.show({
           controller: 'AddEntryController',
@@ -71,6 +29,87 @@
 
     vm.go = function (path) {
       $location.path(path);
+    }
+
+    /**
+     * The following code is for loading
+     * recent, global activity.
+     */
+    vm.recent = [];
+
+    vm.loadRecent = function () {
+      _entryService.getRecent(10)
+        .then(function (response) {
+          vm.recent = response.data;
+        })
+        .catch(function (err) {
+          if (BROADCAST.loggingLevel == "DEBUG") {
+            $rootScope.$broadcast(BROADCAST.error, JSON.stringify(err));
+          } else {
+            $rootScope.$broadcast(BROADCAST.error, "There was an error loading recent questions. Please try again...");
+          }
+        });
+    }
+
+    /**
+     * The following code is for
+     * loading user queries.
+     * (uses debounce)
+     */
+    vm.results = [];
+
+    vm.isSearching = false;
+
+    vm.search = "";
+
+    $scope.$watch('vm.search', function (newV, oldV) {
+      console.log("query", newV);
+      if (newV == '') {
+        vm.results = [];
+        vm.isSearching = false;
+      } else {
+        vm.isSearching = true;
+        vm.query(newV);
+      }
+    });
+
+    var pendingSearch, cancelSearch = angular.noop;
+    var cachedQuery, lastSearch;
+
+    function refreshDebounce() {
+      lastSearch = 0;
+      pendingSearch = null;
+      cancelSearch = angular.noop;
+    }
+
+    function debounceSearch() {
+      var now = new Date().getMilliseconds();
+      lastSearch = lastSearch || now;
+
+      return ((now - lastSearch) < 50000);
+    }
+
+    function preformQuery() {
+      _entryService.query(cachedQuery)
+        .then(function (response) {
+          vm.results = response.data;
+          refreshDebounce();
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+
+    vm.query = function (query) {
+      cachedQuery = query;
+      if (!pendingSearch || !debounceSearch()) {
+        cancelSearch();
+
+        pendingSearch = $q(function (resolve, reject) {
+          cancelSearch = reject;
+          resolve(preformQuery());
+        });
+      }
     }
 
     vm.loadRecent();
