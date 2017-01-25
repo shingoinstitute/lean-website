6b5c0d122e7bf38e3287b777ba4ecc893527a2ad
Craig Blackburn <cr.blackburn89@gmail.com>
2017-01-25 16:55:53 -0700

    "edit" button hide behavior on entries/comments

--- assets/js/controllers/EntryDetailController.js
+++ assets/js/controllers/EntryDetailController.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('leansite')
-    .controller('EntryDetailController', ['$scope', '$routeParams', '_entryService', 'BROADCAST', EntryDetailController]);
+    .controller('EntryDetailController', ['$scope', '$rootScope','$routeParams', '_entryService', 'BROADCAST', EntryDetailController]);
 
-  function EntryDetailController($scope, $routeParams, _entryService, BROADCAST) {
+  function EntryDetailController($scope, $rootScope, $routeParams, _entryService, BROADCAST) {
     var vm = this;
     var id = $routeParams.id;
 
@@ -13,6 +13,10 @@
         .then(function (response) {
           vm.question = response.data;
           vm.question.votes = (vm.question.users_did_upvote.length + 1) - (vm.question.users_did_downvote.length + 1);
+          vm.question.canEdit = response.data.owner.uuid == $rootScope.userId;
+          for (var i = 0; i < vm.question.answers.length; i++) {
+            vm.question.answers[i].canEdit = vm.question.answers[i].owner == $rootScope.userId;
+          }
         })
         .catch(function (err) {
           if (BROADCAST.loggingLevel == "DEBUG") {
