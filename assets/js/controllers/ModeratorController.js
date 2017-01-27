be46b4b4c94ae3f6d93641ffbc04858ef24e8902
Craig Blackburn <cr.blackburn89@gmail.com>
2017-01-27 16:23:05 -0700

    moderatorUser directive

--- assets/js/controllers/ModeratorController.js
+++ assets/js/controllers/ModeratorController.js
@@ -6,37 +6,24 @@
 	ModeratorController.$inject = ['$scope', '$rootScope', '$http', '_userService', '_entryService'];
 
 	function ModeratorController($scope, $rootScope, $http, _userService, _entryService, users) {
-		
-		$scope.questions = [];
-		$scope.answers = [];
-		$scope.comments = [];
-		$scope.users = [];
 
-		function getUsers() {
+		loadData();
+
+		function loadData() {
 			$http.get('/user')
 			.then(function(response) {
 				$scope.users = response.data;
+				return $http.get('/entry?populate=owner,parent');
 			})
-			.catch(function(response) {
-				console.error(response);
-			});
-		}
-
-		function getEntries() {
-			$http.get('/entry?populate=owner,parent')
 			.then(function(response) {
+				$scope.questions = [];
+				$scope.answers = [];
 				for (var i = 0; i < response.data.length; i++) {
 					if (response.data[i].parent) $scope.answers.push(response.data[i]);
 					else $scope.questions.push(response.data[i]);
 				}
+				return $http.get('/comment?populate=owner,parent');
 			})
-			.catch(function(response) {
-				console.error(response);
-			});
-		}
-
-		function getComments() {
-			$http.get('/comment?populate=owner,parent')
 			.then(function(response) {
 				$scope.comments = response.data;
 			})
@@ -45,9 +32,42 @@
 			});
 		}
 
-		getEntries();
-		getComments();
-		getUsers();
+		$scope.saveUser = _userService.updateUser;
+
+		$scope.getUserQuestions = function(user) {
+			_entryService.getUserQuestions(user.uuid)
+			.then(function(response) {
+				console.log('loaded questions...', response.data);
+				user.questions = response.data;
+			})
+			.catch(function(response) {
+				console.error(response);
+			});
+		}
+
+		$scope.getUserAnswers = function(user) {
+			_entryService.getUserAnswers(user.uuid)
+			.then(function(response) {
+				console.log('loaded answers...', response.data)
+				user.answers = response.data;
+			})
+			.catch(function(err) {
+				console.error(response);
+			});
+		}
+
+		$scope.getUserComments = function(user) {
+			_entryService.getUserComments(user.uuid)
+			.then(function(response){
+				console.log('loaded comments...', response.data);
+				user.comments = response.data;
+			})
+			.catch(function(response) {
+				console.error(response);
+			});
+		}
+
+		
 
 	}
 
--- assets/js/directives/ModeratorDirective.js
+++ assets/js/directives/ModeratorDirective.js
@@ -10,6 +10,44 @@
 			controller: 'ModeratorController',
 			controllerAs: 'vm'
 		}
+	})
+	.directive('moderatorUser', function() {
+		return {
+			restrict: 'EA',
+			scope: {
+				user: "="
+			},
+			templateUrl: 'templates/moderator/moderator.user.tmpl.html',
+			controller: 'ModeratorController',
+			controllerAs: 'vm'
+		}
+	})
+	.directive('moderatorAnswer', function() {
+		return {
+			scope: {
+				answer: "="
+			},
+			templateUrl: 'templates/moderator/moderator.answer.tmpl.html',
+			controller: 'ModeratorController'
+		}
+	})
+	.directive('moderatorQuestion', function() {
+		return {
+			scope: {
+				question: "="
+			},
+			templateUrl: 'templates/moderator/moderator.question.tmpl.html',
+			controller: 'ModeratorController'
+		}
+	})
+	.directive('moderatorComment', function() {
+		return {
+			scope: {
+				comment: "="
+			},
+			templateUrl: 'templates/moderator/moderator.comment.tmpl.html',
+			controller: 'ModeratorController'
+		}
 	});
 
 })();
