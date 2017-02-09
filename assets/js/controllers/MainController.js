e308d69318ca4ef983fb7d2a94ce79c8b477a5a9
Craig Blackburn <cr.blackburn89@gmail.com>
2017-02-08 17:14:51 -0700

    sidenav fixes, layout updates

--- assets/js/controllers/MainController.js
+++ assets/js/controllers/MainController.js
@@ -1,8 +1,8 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular.module('leansite')
-    .controller('MainController', MainController);
+	angular.module('leansite')
+		.controller('MainController', MainController);
 
 	MainController.$inject = ['$scope', '$rootScope', '$http', '$cookies', '$location', '$mdMedia', '_userService', 'BROADCAST', 'JWT_TOKEN'];
 
@@ -14,14 +14,16 @@
 		 */
 		vm.getUser = function () {
 			_userService.getUser()
-			.then(function(response) {
-				vm.user = response.data;
-				$rootScope.userId = response.data.uuid;
-			});
+				.then(function (response) {
+					vm.user = response.data;
+					$rootScope.userId = response.data.uuid;
+				});
 		};
 
-		vm.toggleSidenav = function() {
-			$rootScope.$broadcast('toggle-sidenav');
+		vm.toggleSidenav = function () {
+			if (!vm.sideNavLocked) {
+				$rootScope.$broadcast('toggle-sidenav');
+			}
 		}
 
 		/**
@@ -30,24 +32,26 @@
 		$scope.$watch(function () {
 			return $mdMedia('gt-sm');
 		}, function (isBigEnough) {
-			vm.sideNavEnabled = isBigEnough;
+			vm.sideNavLocked = isBigEnough;
 		});
 
 		/**
 		 * @desc :: listener for BROADCAST.error, displays error message when invoked
 		 */
 		$scope.$on(BROADCAST.error, function (event, args) {
-			if (args.data && args.data.error) {
-				args = args.data.error;
-			} else if (args.error) {
-				args = args.error;
-			}
-			if (typeof args == 'Error') {
-				vm.error = args.message;
-			} else if (typeof args == 'string') {
-				vm.error = args;
-			} else {
-				vm.error = JSON.stringify(args, null, 2);
+			if ($BROADCAST.loggingLevel == "DEBUG") {
+				if (args.data && args.data.error) {
+					args = args.data.error;
+				} else if (args.error) {
+					args = args.error;
+				}
+				if (typeof args == 'Error') {
+					vm.error = args.message;
+				} else if (typeof args == 'string') {
+					vm.error = args;
+				} else {
+					vm.error = JSON.stringify(args, null, 2);
+				}
 			}
 		});
 
@@ -61,7 +65,7 @@
 		/**
 		 * @desc :: listener for user login
 		 */
-		$scope.$on(BROADCAST.userLogin, function(event, user) {
+		$scope.$on(BROADCAST.userLogin, function (event, user) {
 			vm.user = user;
 		});
 
@@ -81,58 +85,58 @@
 		 * 
 		 * If no controller name is passed in as the second argument to the listener function, the default behavior for MainController is to retrieve the user from the server.
 		 */
-		$scope.$on('$MainControllerListener', function(event, controller) {
+		$scope.$on('$MainControllerListener', function (event, controller) {
 			switch (controller) {
 				case 'NavController':
-				$rootScope.$broadcast('$NavControllerListener', vm.user);
-				break;
+					$rootScope.$broadcast('$NavControllerListener', vm.user);
+					break;
 				case 'DashboardController':
-				if (vm.user) {
-					$rootScope.$broadcast('$DashboardControllerListener', vm.user);
-				} else {
-					_userService.getUser()
-					.then(function(response) {
-						if (response.error) { $rootScope.$broadcast(BROADCAST.error, response.error); }
-						vm.user = response.data;
+					if (vm.user) {
 						$rootScope.$broadcast('$DashboardControllerListener', vm.user);
-					})
-					.catch(function(err) {
-						$rootScope.$broadcast(BROADCAST.error, err);
-					});
-				}
-				break;
+					} else {
+						_userService.getUser()
+							.then(function (response) {
+								if (response.error) { $rootScope.$broadcast(BROADCAST.error, response.error); }
+								vm.user = response.data;
+								$rootScope.$broadcast('$DashboardControllerListener', vm.user);
+							})
+							.catch(function (err) {
+								$rootScope.$broadcast(BROADCAST.error, err);
+							});
+					}
+					break;
 				case 'AuthController':
-				
-				break;
+
+					break;
 				case 'SettingsController':
-				// do something...
-				break;
+					// do something...
+					break;
 				case 'QuestionController':
-				if (vm.user) {
-					$rootScope.$broadcast('$QuestionControllerListener', vm.user);
-				} else {
-					_userService.getUser()
-					.then(function(response) {
-						vm.user = response.data;
+					if (vm.user) {
 						$rootScope.$broadcast('$QuestionControllerListener', vm.user);
-					})
-					.catch(function(err) {
-						$rootScope.$broadcast(BROADCAST.error, err);
-					});
-				}
-				break;
+					} else {
+						_userService.getUser()
+							.then(function (response) {
+								vm.user = response.data;
+								$rootScope.$broadcast('$QuestionControllerListener', vm.user);
+							})
+							.catch(function (err) {
+								$rootScope.$broadcast(BROADCAST.error, err);
+							});
+					}
+					break;
 				case 'TEST':
-				console.log('user: ', vm.user);
-				
-				break;
+					console.log('user: ', vm.user);
+
+					break;
 				default:
-				vm.getUser();
-				break;
+					vm.getUser();
+					break;
 			}
 		});
 
 		vm.getUser();
 
-  }
+	}
 
 })();
