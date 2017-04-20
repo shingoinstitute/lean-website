a5d6b4e5b74cdf8c65d1ef09c49ee6c80c7a6bf9
Craig Blackburn <cr.blackburn89@gmail.com>
2017-04-20 17:19:23 -0600

    caching build files, merging package.json

--- assets/js/build/app/directives/IsSameDirective.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=IsSameDirective.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,18 @@ module.exports = {
     },
     resolve: {
         // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: [".webpack.js", ".web.js", ".ts"]
+        extensions: [".webpack.js", ".web.js", ".ts"],
+        alias: {
+            'rxjs': 'rxjs-es'
+        }
     },
     module: {
         loaders: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { 
+                test: /\.tsx?$/, 
+                loader: "ts-loader"
+            }
         ]
     },
     plugins: [
